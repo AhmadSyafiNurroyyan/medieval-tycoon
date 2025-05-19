aa6eb94de7a0f16adef581e8df2147ec67b9bd11
tauwus <herdy.aja2306@gmail.com>
2025-05-19 12:56:35 +0700

    .

--- tiles/TileManager.java
+++ tiles/TileManager.java
@@ -1,12 +1,9 @@
 package tiles;
 
 import gui.GamePanel;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
 import javax.imageio.ImageIO;
 
 public class TileManager {
