cc1b9cc40c698291b96169081ff97749dcaa9cb9
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-19 10:55:17 +0700

    Update Barang.java

--- model/Barang.java
+++ model/Barang.java
@@ -2,7 +2,7 @@ package model;
 
 import enums.JenisBarang;
 
-public class Barang {
+public class Barang implements Cloneable {
 
     private final JenisBarang jenis;
     private final int hargaBeli;
@@ -17,14 +17,15 @@ public class Barang {
     }
 
     public void kurangiKesegaran() {
-        kesegaran -= 25;
-        if (kesegaran < 0) {
-            kesegaran = 0;
-        }
+        kesegaran = Math.max(0, kesegaran - 25);
     }
 
     public boolean isBusuk() {
         return kesegaran <= 0;
+    }
+
+    public boolean isSegar() {
+        return kesegaran > 50;
     }
 
     public int getKesegaran() {
@@ -49,5 +50,28 @@ public class Barang {
             return true;
         }
         return false;
+    }
+
+    public String getKategori() {
+        return jenis.getKategori();
+    }
+
+    public String getNamaBarang() {
+        return jenis.name();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (Kesegaran: %d%%, Harga Jual: %d)",
+                jenis.name(), kesegaran, hargaJual);
+    }
+
+    @Override
+    public Barang clone() throws CloneNotSupportedException {
+        try {
+            return (Barang) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return new Barang(this.jenis);
+        }
     }
 }
