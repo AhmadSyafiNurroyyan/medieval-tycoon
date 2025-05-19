ee5645841191ab78ab3f1969ba058ef03007607a
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-19 21:43:49 +0700

    Update PerkType.java

--- enums/PerkType.java
+++ enums/PerkType.java
@@ -1,7 +1,18 @@
 package enums;
 
 public enum PerkType {
-    ELEGAN, 
-    CHARMING, 
-    ACTIVE;
+    ELEGAN("Elegan"),
+    CHARMING("Charming"),
+    ACTIVE("Active");
+
+    private final String nama;
+
+    PerkType(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
 }
