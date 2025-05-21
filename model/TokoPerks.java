4975f9b86a122da0916208cd23ba675fe95e60af
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-21 13:01:28 +0700

    add toko perks

---
+++ model/TokoPerks.java
@@ -0,0 +1,19 @@
+
+package model;
+
+import java.util.*;
+import enums.PerkType;
+
+public class TokoPerks {
+    List<PerkType> perks = new ArrayList<>();
+    
+    public TokoPerks() {
+        
+    }
+    
+    public perks getPerks() {
+        
+    }
+    
+    
+}
