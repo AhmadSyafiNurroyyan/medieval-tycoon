9dd29ef4442486a6cf4133180f407f0c83a5370e
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-22 00:28:40 +0700

    Update Supplier.java

--- model/Supplier.java
+++ model/Supplier.java
@@ -1,11 +1,14 @@
 package model;
 
 import enums.JenisBarang;
+import interfaces.Transaksi;
+import interfaces.Showable;
 
 import java.util.*;
 
-public class Supplier {
+public class Supplier implements Transaksi<JenisBarang>, Showable {
 
+    private Player player;
     private final Set<JenisBarang> stokHariIni;
     private final Random random = new Random();
     private final int jumlahMinStokHarian = 5;
@@ -21,7 +24,8 @@ public class Supplier {
         List<JenisBarang> semuaJenis = new ArrayList<>(List.of(JenisBarang.values()));
         Collections.shuffle(semuaJenis);
 
-        int jumlahTersedia = jumlahMinStokHarian + random.nextInt(jumlahMaxStokHarian - jumlahMinStokHarian + 1);
+        int jumlahTersedia = jumlahMinStokHarian
+                + random.nextInt(jumlahMaxStokHarian - jumlahMinStokHarian + 1);
 
         for (int i = 0; i < jumlahTersedia; i++) {
             stokHariIni.add(semuaJenis.get(i));
@@ -36,17 +40,31 @@ public class Supplier {
         return new HashSet<>(stokHariIni);
     }
 
-    public Barang beliBarang(JenisBarang jenis) {
-        if (tersediaHariIni(jenis)) {
-            return new Barang(jenis);
+    @Override
+    public boolean beli(Player player, JenisBarang jenis) {
+        if (!tersediaHariIni(jenis)) {
+            System.out.println("Barang tidak tersedia hari ini.");
+            return false;
         }
-        return null;
+
+        int harga = jenis.getHarga();
+        if (player.getMoney() < harga) {
+            System.out.println("Uang tidak cukup untuk membeli barang.");
+            return false;
+        }
+
+        player.kurangiMoney(harga);
+        player.tambahBarang(new Barang(jenis));
+
+        System.out.println("Berhasil membeli: " + jenis.name());
+        return true;
     }
 
-    public void printStokHariIni() {
+    @Override
+    public void tampilkanDetail() {
         System.out.println("=== Stok Supplier Hari Ini ===");
         for (JenisBarang barang : stokHariIni) {
-            System.out.printf("%s - Harga: %d\n", barang.name(), barang.getHarga());
+            System.out.printf("- %s (Harga: %d)\n", barang.name(), barang.getHarga());
         }
     }
 
