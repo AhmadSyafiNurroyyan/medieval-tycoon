b4a4763d668f97bb2e6c51dc5ff1a32838ee93c8
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-21 13:03:14 +0700

    Update Transaksi.java

--- interfaces/Transaksi.java
+++ interfaces/Transaksi.java
@@ -1,9 +1,13 @@
 package interfaces;
- 
-import enums.JenisItem;
+
 import model.Player;
 
-public interface Transaksi {
+public interface Transaksi<T> {
 
-    public boolean transaksi(Player player, JenisItem item, int jumlah);
+    boolean beli(Player player, T item);
+
+    default boolean jual(Player player, T item) {
+        // Default-nya tidak semua transaksi bisa menjual
+        throw new UnsupportedOperationException("Jual tidak didukung.");
+    }
 }
