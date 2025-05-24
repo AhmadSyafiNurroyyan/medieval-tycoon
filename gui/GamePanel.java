39ef5daa7aaaca84662e010bd37b6476b169791c
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-24 17:26:05 +0700

    Update GamePanel.java

--- gui/GamePanel.java
+++ gui/GamePanel.java
@@ -15,7 +15,7 @@ import tiles.TriggerZoneManager;
 //import debugger.DebugCoordinateLogger;
 
 public class GamePanel extends JPanel implements Runnable {
-    private Player player = new Player("tauwus");
+    private Player player;
     private PlayerMovement playerMovement;
     private PlayerSkin PlayerSkin;
     MapObjectManager mapObjectManager = new MapObjectManager();
@@ -24,23 +24,22 @@ public class GamePanel extends JPanel implements Runnable {
     private TileManager tileManager;
     private Camera camera;
     private TriggerZoneManager triggerZoneManager;
-    private Supplier supplier; 
+    private Supplier supplier;
     private Runnable showSupplierPanelCallback;
     private Runnable showHomeBasePanelCallback;
 
-    
-    
-    public GamePanel() {
+    public GamePanel(Player player) {
+        this.player = player;
         setBackground(Color.BLACK);
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
-        
+
         playerMovement = player.createMovement();
         PlayerSkin = player.createNametag();
         tileManager = new TileManager(this);
         camera = new Camera(this, tileManager);
         triggerZoneManager = new TriggerZoneManager();
-        supplier = new Supplier(); 
+        supplier = new Supplier();
         triggerZoneManager.addZone("supplier", 511, 480, 1054, 575, true, () -> {
             if (showSupplierPanelCallback != null) {
                 SwingUtilities.invokeLater(showSupplierPanelCallback);
@@ -51,42 +50,44 @@ public class GamePanel extends JPanel implements Runnable {
                 SwingUtilities.invokeLater(showHomeBasePanelCallback);
             }
         });
-        
 
         // mapObjectManager.addObject("assets/sprites/objects/tree.png", 128, 96);
-        // mapObjectManager.addObject("assets/sprites/objects/rock.png", 200, 150); 
+        // mapObjectManager.addObject("assets/sprites/objects/rock.png", 200, 150);
         int xshop = 935, yshop = 545;
         mapObjectManager.addObject("assets/sprites/objects/house.png", 80, 30, true);
         mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop, yshop, true);
-        mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop-140, yshop, true);
-        mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop-140-140, yshop, true);
-        mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop-140-140-140, yshop, true);
+        mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop - 140, yshop, true);
+        mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop - 140 - 140, yshop, true);
+        mapObjectManager.addObject("assets/sprites/objects/shop.png", xshop - 140 - 140 - 140, yshop, true);
         mapObjectManager.addObject("assets/sprites/objects/tent.png", 230, 1280, true);
 
         addKeyListener(new KeyAdapter() {
-            @Override 
+            @Override
             public void keyPressed(KeyEvent e) {
                 playerMovement.keyPressed(e.getKeyCode());
                 if (e.getKeyCode() == KeyEvent.VK_E) {
-                    List<tiles.TriggerZoneManager.TriggerZone> zones = triggerZoneManager.getZonesAt(playerMovement.getX(), playerMovement.getY());
+                    List<tiles.TriggerZoneManager.TriggerZone> zones = triggerZoneManager
+                            .getZonesAt(playerMovement.getX(), playerMovement.getY());
                     for (tiles.TriggerZoneManager.TriggerZone zone : zones) {
                         zone.trigger();
                     }
                 }
             }
-            @Override 
+
+            @Override
             public void keyReleased(KeyEvent e) {
                 playerMovement.keyReleased(e.getKeyCode());
             }
         });
-        
+
         // no stuck keys
         addFocusListener(new FocusAdapter() {
-            @Override public void focusLost(FocusEvent e) {
+            @Override
+            public void focusLost(FocusEvent e) {
                 playerMovement.resetKeys();
             }
         });
-        
+
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -95,7 +96,7 @@ public class GamePanel extends JPanel implements Runnable {
                 debugger.DebugClickLogger.logClickCoordinates(e, mapX, mapY);
             }
         });
-        
+
     }
 
     @Override
@@ -116,26 +117,28 @@ public class GamePanel extends JPanel implements Runnable {
             oldThread.interrupt();
         }
     }
-    
+
     @Override
     public void run() {
         final double drawInterval = 1000000000.0 / FPS;
         double nextDrawTime = System.nanoTime() + drawInterval;
         // boolean wasInZone = false;
         while (gameThread != null) {
-            playerMovement.update(tileManager.getMapWidth(), tileManager.getMapHeight(), tileManager.getTileSize(), mapObjectManager, tileManager);
+            playerMovement.update(tileManager.getMapWidth(), tileManager.getMapHeight(), tileManager.getTileSize(),
+                    mapObjectManager, tileManager);
 
-            // boolean inZone = !triggerZoneManager.getZonesAt(playerMovement.getX(), playerMovement.getY()).isEmpty();
+            // boolean inZone = !triggerZoneManager.getZonesAt(playerMovement.getX(),
+            // playerMovement.getY()).isEmpty();
             // if (inZone && !wasInZone) {
-            //     System.out.println("[DEBUG] Entered trigger zone");
+            // System.out.println("[DEBUG] Entered trigger zone");
             // } else if (!inZone && wasInZone) {
-            //     System.out.println("[DEBUG] Exited trigger zone");
+            // System.out.println("[DEBUG] Exited trigger zone");
             // }
             // wasInZone = inZone;
             repaint();
             try {
                 double remaining = nextDrawTime - System.nanoTime();
-                long sleepMs = Math.max(0, (long)(remaining / 1_000_000));
+                long sleepMs = Math.max(0, (long) (remaining / 1_000_000));
                 if (sleepMs > 0) {
                     Thread.sleep(sleepMs);
                 }
@@ -145,7 +148,8 @@ public class GamePanel extends JPanel implements Runnable {
                 break;
             }
         }
-    }    
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -171,7 +175,7 @@ public class GamePanel extends JPanel implements Runnable {
         int goldWidth = fm.stringWidth(goldText);
         int goldX = getWidth() - goldWidth - 20;
         int goldY = 40;
-        g2d.setColor(new Color(0,0,0,150));
+        g2d.setColor(new Color(0, 0, 0, 150));
         g2d.fillRoundRect(goldX - 10, goldY - fm.getAscent(), goldWidth + 20, fm.getHeight() + 8, 12, 12);
         g2d.setColor(new Color(218, 165, 32));
         g2d.drawString(goldText, goldX, goldY);
@@ -184,7 +188,7 @@ public class GamePanel extends JPanel implements Runnable {
         int uidWidth = fm.stringWidth(uidText);
         int uidX = getWidth() - uidWidth - 20;
         int uidY = getHeight() - 20;
-        g2d.setColor(new Color(0,0,0,150));
+        g2d.setColor(new Color(0, 0, 0, 150));
         g2d.fillRoundRect(uidX - 10, uidY - fm.getAscent(), uidWidth + 20, fm.getHeight() + 8, 12, 12);
         g2d.setColor(Color.WHITE);
         g2d.drawString(uidText, uidX, uidY);
@@ -193,17 +197,20 @@ public class GamePanel extends JPanel implements Runnable {
     public Player getPlayer() {
         return player;
     }
+
     public TileManager getTileManager() {
         return tileManager;
     }
+
     public void setShowSupplierPanelCallback(Runnable cb) {
         this.showSupplierPanelCallback = cb;
     }
+
     public void setShowHomeBasePanelCallback(Runnable cb) {
         this.showHomeBasePanelCallback = cb;
     }
+
     public Supplier getSupplier() {
         return supplier;
     }
 }
-
