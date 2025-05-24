0c209ca03e6028b3aee913a01d14f60f701e21a5
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-24 17:25:40 +0700

    Update main.java

--- gui/main.java
+++ gui/main.java
@@ -2,17 +2,24 @@ package gui;
 
 import javax.swing.SwingUtilities;
 
+import model.Player;
+
 public class main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(MainMenu::new);
+        Player player = new Player("Tauwus");
+
+        SwingUtilities.invokeLater(() -> {
+            MainMenu menu = new MainMenu(player);
+            menu.setVisible(true);
+        });
         // SwingUtilities.invokeLater(() -> {
-        //     JFrame frame = new JFrame("Medieval Tycoon");
-        //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //     frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //     frame.setSize(800, 600); 
-        //     frame.add(new HomeBasePanel());
-        //     frame.setLocationRelativeTo(null);
-        //     frame.setVisible(true); 
+        // JFrame frame = new JFrame("Medieval Tycoon");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        // frame.setSize(800, 600);
+        // frame.add(new HomeBasePanel());
+        // frame.setLocationRelativeTo(null);
+        // frame.setVisible(true);
         // });
     }
 }
