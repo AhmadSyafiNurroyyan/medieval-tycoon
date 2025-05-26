ba43dcf7698d3332e05fe09e1dc7ef83189c995d
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-26 21:50:33 +0700

    belum nyimpan list perks.java

--- model/Inventory.java
+++ model/Inventory.java
@@ -1,124 +1,86 @@
 package model;
 
-import java.util.*;
+import interfaces.Showable;
+import interfaces.Upgrade;
 
-public class Inventory {
+public class Gerobak implements Upgrade, Showable {
 
-    private final Map<Barang, Integer> stokBarang;
-    private final Map<Item, Integer> stokItem;
-    private final Map<Barang, Integer> barangDibawa;
-    private final Map<Barang, Integer> hargaJualBarang;
+    private int level;
+    private static final int KAPASITAS_PERKS = 2;
+    private static final int MAX_LEVEL = 5;
 
-    public Inventory() {
-        this.stokBarang = new HashMap<>();
-        this.stokItem = new HashMap<>();
-        this.barangDibawa = new HashMap<>();
-        this.hargaJualBarang = new HashMap<>();
+    public Gerobak() {
+        this.level = 0;
     }
 
-    // Tambah barang ke stok
-    public void tambahBarang(Barang barang) {
-        stokBarang.put(barang, stokBarang.getOrDefault(barang, 0) + 1);
+    @Override
+    public int getLevel() {
+        return level;
     }
 
-    // Tambah item ke stok
-    public void tambahItem(Item item) {
-        stokItem.put(item, stokItem.getOrDefault(item, 0) + 1);
-    }
-
-    // Ambil semua barang dari stok
-    public List<Barang> getStokBarang() {
-        List<Barang> result = new ArrayList<>();
-        for (Map.Entry<Barang, Integer> entry : stokBarang.entrySet()) {
-            for (int i = 0; i < entry.getValue(); i++) {
-                result.add(entry.getKey());
-            }
-        }
-        return result;
-    }
-
-    // Ambil semua item dari stok
-    public List<Item> getStokItem() {
-        List<Item> result = new ArrayList<>();
-        for (Map.Entry<Item, Integer> entry : stokItem.entrySet()) {
-            for (int i = 0; i < entry.getValue(); i++) {
-                result.add(entry.getKey());
-            }
-        }
-        return result;
-    }
-
-    // Hapus barang busuk dari stok
-    public void bersihkanBarangBusuk() {
-        Iterator<Map.Entry<Barang, Integer>> iterator = stokBarang.entrySet().iterator();
-        while (iterator.hasNext()) {
-            Map.Entry<Barang, Integer> entry = iterator.next();
-            if (entry.getKey().isBusuk()) {
-                iterator.remove();
-            }
+    public int getKapasitasBarang() {
+        switch (level) {
+            case 0:
+                return 20;
+            case 1:
+                return 30;
+            case 2:
+                return 45;
+            case 3:
+                return 60;
+            case 4:
+                return 75;
+            case 5:
+                return 90;
+            default:
+                return 20;
         }
     }
 
-    // Kurangi kesegaran semua barang
-    public void kurangiKesegaranSemua() {
-        for (Barang barang : stokBarang.keySet()) {
-            barang.kurangiKesegaran();
+    public int getKapasitasItem() {
+        if (level <= 1) {
+            return 1;
+        } else if (level <= 3) {
+            return 2;
+        } else {
+            return 3;
         }
     }
 
-    // Hapus 1 buah barang dari stok
-    public boolean hapusBarang(Barang barang) {
-        if (stokBarang.containsKey(barang)) {
-            int count = stokBarang.get(barang);
-            if (count > 1) {
-                stokBarang.put(barang, count - 1);
-            } else {
-                stokBarang.remove(barang);
-            }
+    public int getKapasitasPerks() {
+        return KAPASITAS_PERKS;
+    }
+
+    public int getBiayaUpgrade() {
+        if (isMaxLevel()) {
+            return -1;
+        }
+        return 100_000 + (level * level * 25_000);
+    }
+
+    public boolean isMaxLevel() {
+        return this.level == MAX_LEVEL;
+    }
+
+    @Override
+    public boolean upgradeLevel() {
+        if (!isMaxLevel()) {
+            level++;
             return true;
         }
         return false;
     }
 
-    // Total semua barang
-    public int getJumlahBarang() {
-        int total = 0;
-        for (int count : stokBarang.values()) {
-            total += count;
+    @Override
+    public void tampilkanDetail() {
+        System.out.println("Level: " + level);
+        System.out.println("Kapasitas Barang: " + getKapasitasBarang());
+        System.out.println("Kapasitas Item: " + getKapasitasItem());
+        System.out.println("Kapasitas Perks: " + KAPASITAS_PERKS);
+        if (!isMaxLevel()) {
+            System.out.println("Biaya Upgrade ke level " + (level + 1) + ": " + getBiayaUpgrade());
+        } else {
+            System.out.println("Gerobak sudah mencapai level maksimal.");
         }
-        return total;
-    }
-
-    // Set barang dan jumlah yang akan dibawa berjualan
-    public void bawaBarang(Barang barang, int jumlah, int kapasitasGerobak) {
-        if (jumlah <= kapasitasTersisa(kapasitasGerobak) && stokBarang.getOrDefault(barang, 0) >= jumlah) {
-            barangDibawa.put(barang, barangDibawa.getOrDefault(barang, 0) + jumlah);
-            stokBarang.put(barang, stokBarang.get(barang) - jumlah);
-            if (stokBarang.get(barang) <= 0) {
-                stokBarang.remove(barang);
-            }
-        }
-    }
-
-    // Kapasitas tersisa di gerobak
-    public int kapasitasTersisa(int kapasitasGerobak) {
-        int total = 0;
-        for (int val : barangDibawa.values()) {
-            total += val;
-        }
-        return kapasitasGerobak - total;
-    }
-
-    // Atur harga jual barang
-    public void setHargaJual(Barang barang, int harga) {
-        hargaJualBarang.put(barang, harga);
-    }
-
-    public int getHargaJual(Barang barang) {
-        return hargaJualBarang.getOrDefault(barang, 0);
-    }
-
-    public Map<Barang, Integer> getBarangDibawa() {
-        return Collections.unmodifiableMap(barangDibawa);
     }
 }
