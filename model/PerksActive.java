204f277437667e52a7dd961c748fa64f8c5f02f2
Ahmad Syafi Nurroyyan <ryansyafi@student.ub.ac.id>
2025-05-28 01:21:44 +0700

    Update PerksActive.java

--- model/PerksActive.java
+++ model/PerksActive.java
@@ -6,11 +6,12 @@ public class PerksActive extends Perk {
 
     public PerksActive() {
         super(PerkType.ACTIVE.getNama(), "Meningkatkan peluang untuk bertemu pembeli.", PerkType.ACTIVE, 500_000, 1.0,
-                200_000);
+                200_000, "active.png");
     }
 
     public PerksActive(PerksActive other) {
-        super(other.nama, other.deskripsi, other.type, other.harga, other.kesaktianAwal, other.biayaUpgrade);
+        super(other.nama, other.deskripsi, other.type, other.harga, other.kesaktianAwal, other.biayaUpgrade,
+                other.iconPath);
         this.level = other.level;
         this.kesaktianSekarang = other.kesaktianSekarang;
         this.isActive = other.isActive;
